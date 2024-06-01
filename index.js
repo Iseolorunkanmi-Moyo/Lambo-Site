30516841604a7312b5572cc2ba1b42168f839ac4
Oluwatomisin <kanmitomi@gmail.com>
2024-06-01 12:50:57 +0100

    first commit

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var searchTrigger = document.getElementById("searchTrigger")
 var menu = document.getElementById("menu")
+var menuImg = document.getElementById("menuImg")
 var menuDiv = document.getElementById("menuDiv")
+var menuSpace = document.getElementById("closeMenuSpace")
 var returnNav = document.getElementById("returnNav")
 var searchInput = document.getElementById("searchInput")
 var searchBtn = document.getElementById("searchBtn")
 var searchDiv = document.getElementById("searchDiv")
 
 searchTrigger.addEventListener("click", () => {
+   openSearchDiv()
+})
+
+function openSearchDiv(){
     searchDiv.style.display = "flex"
     setTimeout(() => {
         searchInput.focus()
     }, 0.5);
-})
+}
 
 searchInput.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
@@ -27,38 +33,44 @@ returnNav.addEventListener("click", () => {
 
 searchBtn.addEventListener("click", searchFunc)
 
-
-function searchFunc() {
-
-}
+var menucount = 0;
 
 menu.addEventListener("click", () => {
 
-    if (menuDiv.style.display = "none") {
-        menuDiv.style.animationName = "menuSlide"
-        menuDiv.style.animationDuration = "1s"
-        menuDiv.style.display = "flex"
+    if (menucount == 0) {
+        openMenu()
     }
 
-   else if (menuDiv.style.display = "flex") {
-        menuDiv.style.animationName = "reverseMenuSlide"
-        menuDiv.style.animationDuration = "1s"
-        setTimeout(() => {
-            menuDiv.style.display = "none"
-
-        }, 1000);
+    else if (menucount == 1) {
+        closeMenu()
     }
-
 
 })
 
+menuSpace.addEventListener("click", () => {
+    closeMenu()
+})
 
-menuDiv.addEventListener("click", () => {
+function openMenu() {
+    menuDiv.style.animationName = "menuSlide"
+    menuDiv.style.animationDuration = "1s"
+    menuDiv.style.display = "flex"
+    menuImg.setAttribute("src", "./close x.svg")
+    menucount = 1
+}
+
+function closeMenu() {
     menuDiv.style.animationName = "reverseMenuSlide"
     menuDiv.style.animationDuration = "1s"
     setTimeout(() => {
         menuDiv.style.display = "none"
+        menuImg.setAttribute("src", "./menu icon.svg")
+        menucount = 0
 
-    }, 1000);
+    }, 950);
+}
 
-})
+
+function searchFunc() {
+
+}
